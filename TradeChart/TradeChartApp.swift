8f075acc4a43fb10d9bfa08bb7419a709f3648e1
SanjaySiddharth <mjsanjaysiddharth1999@gmail.com>
2024-02-25 08:56:25 +0530

    Initial Commit

---
+++ TradeChart/TradeChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  TradeChartApp.swift
+//  TradeChart
+//
+//  Created by Sanjay Siddharth on 25/02/24.
+//
+
+import SwiftUI
+
+@main
+struct TradeChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
